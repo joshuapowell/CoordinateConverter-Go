f03b13b3d17789c151596b950f4d77da4afb51cb
Joshua Powell <joshuapowell@users.noreply.github.com>
2023-02-16 18:23:29 -0500

    Changed expected answer

--- coordinate_converter_test.go
+++ coordinate_converter_test.go
@@ -4,8 +4,8 @@ import "testing"
 
 func TestDmsToDd(t *testing.T) {
 
-	want := 30.2638888888
 	got := DmsToDd(30, 15, 50)
+	want := 30.26388888888889
 
 	if got != want {
 		t.Errorf("got %g, wanted %g", got, want)
