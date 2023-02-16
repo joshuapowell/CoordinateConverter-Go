0ea789c32cb30a83ebc471e9faebbbc3eccb16e7
Joshua Powell <joshuapowell@users.noreply.github.com>
2023-02-16 18:07:02 -0500

    Created simple task list for function

--- coordinate_converter.go
+++ coordinate_converter.go
@@ -9,8 +9,20 @@ type CoordinateDMS struct {
 /**
  * Convert Degrees Minutes Seconds (DMS) to Decimal Degrees (DD)
  *
- *
+ * @todo document this function's parameters and return value
  *
  */
 func DmsToDd(degree string, minute string, second string) float64 {
+
+	// @todo check for valid inputs
+
+	// @todo assign valid inputs to the CoordinateDMS struct
+
+	// @todo convert the struct to a single decimal (float64)
+
+	// @todo make sure that the converted decimal is between the max and min
+	//       latitude and longitude
+
+	// @todo return the newly converted decimal coordinate
+	return 40.123456789
 }
