8342346f6ba3a986585a9b674bc023c3677916a5
Joshua Powell <joshuapowell@users.noreply.github.com>
2023-02-21 17:06:02 -0500

    Updated documentation and data type for Coordinates

--- coordinate.struct.go
+++ coordinate.struct.go
@@ -1,9 +1,13 @@
 package coordinate_converter
 
 /**
- * A Coordinate
+ * Coordinate defines a data structure that represents a coordinate pair.
+ *
+ * The coordinate pair consists of a `Latitude` value between -90.0 and 90.0
+ * and a longitude value between -180.0 and 180.0
+ *
  */
 type Coordinate struct {
-	Latitude  string `validate:"required,string,min=-90,max=90"`
-	Longitude string `validate:"required,string,min=-180,max=180"`
+	Latitude  float64 `validate:"required,string,min=-90,max=90"`
+	Longitude float64 `validate:"required,string,min=-180,max=180"`
 }
